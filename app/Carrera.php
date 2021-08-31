b9b68827a96261c29de9bfa0bb4e4dab948ab78c
Yetson <2056959752@qq.com>
2021-08-31 18:39:03 -0500

    first commit

---
+++ app/Carrera.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Carrera extends Model
+{
+    //
+    public $table = "carrera";
+    protected $primarykey = "idcarrera";
+}
---
+++ app/Unidad.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Unidad extends Model
+{
+    //
+    public $table = 'unidad_didactica';
+    protected $primarykey = 'idunidad_didactica';
+}
